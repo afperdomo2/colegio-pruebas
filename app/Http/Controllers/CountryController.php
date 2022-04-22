2bee9f2a83fdeac1e906920d3c5cc9b3d911a676
Andrés Perdomo <alvarado0925@gmail.com>
2022-04-21 23:03:24 -0500

    🧪Feat (countries): Agrega middleware para requerir una sesión de usuario iniciada

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -7,6 +7,11 @@ use App\Models\Country;
 
 class CountryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
